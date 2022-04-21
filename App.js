9b44ee6ac79d1b6fc68f50e3fe1736ff3dc0a043
WebOsmotic M1 <webosmoticdeveloper@WebOsmotics-Mac-mini-3.local>
2022-04-21 13:40:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Display from './src/components/Display';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Display />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/components/Display.js
@@ -0,0 +1,17 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import axios from 'axios';
+
+const Display = () => {
+  //   const data = axios.getUri('https://jsonplaceholder.typicode.com/posts');
+  //   console.log(data);
+  return (
+    <View>
+      <Text>Display</Text>
+    </View>
+  );
+};
+
+export default Display;
+
+const styles = StyleSheet.create({});
